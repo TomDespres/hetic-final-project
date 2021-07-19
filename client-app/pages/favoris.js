b1f884e9d07c5c008b4d649439e2a250eca460fb
Tom Despres <tom.despres@castoretpollux.com>
2021-07-19 15:57:13 +0200

    fix

--- client-app/pages/favoris.js
+++ client-app/pages/favoris.js
@@ -4,10 +4,7 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a href="https://vite-ma-table.vercel.app/">Vite Ma Table</a>
-        </h1>
-        <a href="/">page home</a>
+        <code>Page: favoris</code>
       </main>
     </div>
   )
--- client-app/pages/index.js
+++ client-app/pages/index.js
@@ -5,10 +5,7 @@ export default function Home() {
   return (
     <div className={styles.wrapper}>
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          <a href="https://vite-ma-table.vercel.app/">Vite Ma Table</a>
-        </h1>
-        <a href="/favoris">Voir mes salles favorites</a>
+        <code>Page: home</code>
       </main>
     </div>
   )
